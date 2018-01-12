496b3fe6772db08819d92312732c517596722c90
Rolando Castillo <maduron@yahoo.com>
2018-01-11 21:04:06 -0600

    Add files via upload

--- csv2json.js
+++ csv2json.js
@@ -5,7 +5,7 @@ if (process.argv.length !== 4) {
     console.log('The first line should contain member "names" for all json objects.');
     console.log('Subsequent lines should have member "values" for each json object.');
     console.log('Usage:');
-    console.log('       node csv2json.js <input-csv-file> <output-json-file>');
+    console.log('       node csv2json.js input-csv-file output-json-file');
     console.log('------------------------------------------------------------------');
     process.exit(-1);
 }
@@ -27,7 +27,7 @@ const reader = readline.createInterface({
 });
 
 const out = fs.createWriteStream(outFile);
-out.write('[\n');
+out.write('[\r\n');  // every '\r\n' sequence is to match provided output (in windows style)
 
 let nl = 0;
 let names;
@@ -39,14 +39,22 @@ reader.on('line', (line) => {
     if (nl === 1) { // get the names from the first line...
         names = line.split(',');
         len = names.length;
-    } else {        // for every line subsequent line, get its values...
+    } else {        // for every subsequent line, get its values...
         vals = line.split(',');
         if (vals.length === len) {
             let obj = {};  // object to be stringified...
             for (let i = 0; i < len; ++i) {
                 obj[names[i]] = vals[i];
             }
-            out.write(JSON.stringify(obj) + "\n");
+            let objStr = JSON.stringify(obj);
+            const s1 = objStr.replace('{"', '  {\r\n    "'); // to match provided output
+            const s2 = s1.replace(/":"/g, '": "');         // to match provided output
+            const s3 = s2.replace(/","/g, '",\r\n    "');     // to match provided output
+            objStr = s3.replace('"}', '"\r\n  }');           // to match provided output
+            if (nl > 2) {
+                out.write(',\r\n') // separate from previous object...
+            }
+            out.write(objStr);
         } else {
             console.log(`Line ${nl} with ${vals.length} values; ${len} are required. Skipping...`);
         }
@@ -54,7 +62,10 @@ reader.on('line', (line) => {
 });
 
 reader.on('close', () => {
-    out.write(']\n');
+    if (nl > 2) {
+        out.write('\r\n');   // if at least one "values" line, separate last one without a comma...
+    }
+    out.write(']');
     out.close();
-    console.log(`Number of lines processed = ${nl}.`);
+    console.log(`Number of processed lines = ${nl}.`);
 });
